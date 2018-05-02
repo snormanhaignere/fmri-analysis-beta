5d19af5f026e40ede1fd22be56bfba0dfce2c036
Sam Norman-Haignere <snormanhaignere@gmail.com>
2018-05-02 19:06:22 +0200

    Modified noisecorr_similarity_via_regression_voxelgrid.m
    Made it possible to have different train and test data

--- code/noisecorr_similarity_via_regression_voxelgrid.m
+++ code/noisecorr_similarity_via_regression_voxelgrid.m
@@ -1,4 +1,4 @@
-function [Gsim, similarity_metric_MAT_file] = ...
+function [Gsim, similarity_metric_MAT_file, Gbootstrap, bootstrap_metric_MAT_file] = ...
     noisecorr_similarity_via_regression_voxelgrid(...
     G, F, output_directory, varargin)
 
@@ -12,6 +12,16 @@ function [Gsim, similarity_metric_MAT_file] = ...
 % measures are saved.
 %
 % 2017-08-11: Making within-fold correlation optional
+% 
+% 2017-09-06: Adding bootstrapping
+% 
+% 2017-11-02: Made it possible to pass the actual bootstrapped samples as an
+% optional argument
+% 
+% 2018-03-12: Made it possible to have the training and test data be different
+% by adding optional G_test field
+
+addpath(genpath('/mindhive/nklab/u/svnh/general-analysis-code'));
 
 if ischar(F)
     load(F, 'F');
@@ -41,10 +51,14 @@ I.noisecorr_regmetric = false;
 I.max_run_time_in_min = num2str(60*10);
 I.within_fold_correlation = true;
 I.only_cross_column_corr = false;
+I.n_bootstrap_smps = 0;
+I.bstrap_smps = [];
+I.noise_correct = true;
+I.G_test = struct([]);
 I = parse_optInputs_keyvalue(varargin, I);
 
 % cannot use correlation-based correction for variance-based metric
-if strcmp(I.similarity_metric, 'demeaned-squared-error')
+if any(strcmp(I.similarity_metric, {'demeaned-squared-error', 'normalized-squared-error'}))
     I.correction_method = 'variance-based';
 end
 
@@ -58,7 +72,7 @@ if ~exist(prediction_MAT_file, 'file') || I.overwrite_predictions
     [n_stim, n_rep, n_vox] = size(D); %#ok<ASGLU>
     
     % remove zero or NaN voxels
-    nonempty_voxels = ~(all(all(D < 1e-100,1),2) | all(all(isnan(D),1),2));
+    nonempty_voxels = ~(any(all(D < 1e-100,1),2) | any(any(isnan(D),1),2));
     D = D(:,:,nonempty_voxels);
     n_nonempty_vox = size(D,3);
     
@@ -87,6 +101,7 @@ if ~exist(prediction_MAT_file, 'file') || I.overwrite_predictions
         'max_run_time_in_min', I.max_run_time_in_min, ...
         'overwrite', I.overwrite_predictions);
     
+    % save results
     save(prediction_MAT_file, 'Yh', 'D', 'folds', 'nonempty_voxels',...
         'n_stim', 'n_rep', 'n_vox', 'n_nonempty_vox');
     
@@ -95,8 +110,14 @@ end
 %% Similarity metric
 
 % noise-corrected correlations
-similarity_metric_MAT_file = ...
-    [output_directory '/noise_corrected_' I.similarity_metric '.mat'];
+if I.noise_correct
+    similarity_metric_MAT_file = ...
+        [output_directory '/noise_corrected_' I.similarity_metric '.mat'];
+else
+    similarity_metric_MAT_file = ...
+        [output_directory '/' I.similarity_metric '.mat'];
+    error('Need to finish implementing this code without noise correction');
+end
 
 % delete file if is un-loadable
 if exist(similarity_metric_MAT_file, 'file')
@@ -113,8 +134,14 @@ if ~exist(similarity_metric_MAT_file, 'file') || I.overwrite_correlations
     load(prediction_MAT_file, 'Yh', 'D', 'folds', 'nonempty_voxels', ...
         'n_stim', 'n_rep', 'n_vox', 'n_nonempty_vox');
     
+    if ~isempty(I.G_test)
+        D_test = grid2matrix(I.G_test);
+        D_test = D_test(:, :, nonempty_voxels);
+    else
+        D_test = reshape(D, [n_stim, n_rep, n_nonempty_vox]);
+    end
+    
     % separate out reps / voxels into different dimensions
-    D = reshape(D, [n_stim, n_rep, n_nonempty_vox]);
     Yh = reshape(Yh, [n_stim, n_rep, n_nonempty_vox]);
     
     if ~I.within_fold_correlation
@@ -124,17 +151,21 @@ if ~exist(similarity_metric_MAT_file, 'file') || I.overwrite_correlations
     % compute correlation
     r = nan(n_nonempty_vox,1);
     for i = 1:size(D,3);
-        switch I.correction_method
-            case 'correlation-based'
-                r(i) = normalized_correlation_within_folds(...
-                    D(:,:,i), Yh(:,:,i), folds, 'metric', I.similarity_metric, ...
-                    'only_cross_column_corr', I.only_cross_column_corr);
-            case 'variance-based'
-                r(i) = noise_corrected_similarity_within_folds(...
-                    D(:,:,i), Yh(:,:,i), folds, 'metric', I.similarity_metric,...
-                    'only_cross_column_cov', I.only_cross_column_corr);
-            otherwise
-                error('Switch statement fell through');
+        if I.noise_correct
+            switch I.correction_method
+                case 'correlation-based'
+                    r(i) = normalized_correlation_within_folds(...
+                        D_test(:,:,i), Yh(:,:,i), folds, 'metric', I.similarity_metric, ...
+                        'only_cross_column_corr', I.only_cross_column_corr);
+                case 'variance-based'
+                    r(i) = noise_corrected_similarity_within_folds(...
+                        D_test(:,:,i), Yh(:,:,i), folds, 'metric', I.similarity_metric,...
+                        'only_cross_column_cov', I.only_cross_column_corr);
+                otherwise
+                    error('Switch statement fell through');
+            end
+        else
+            error('Need to implement within-fold error metric without noise correction');
         end
     end
     
@@ -147,6 +178,109 @@ else
     
     load(similarity_metric_MAT_file, 'r');
     
+end
+
+%% Bootstrap similarity metric
+
+if I.n_bootstrap_smps > 0
+    
+    
+    if I.noise_correct
+    bootstrap_metric_MAT_file = ...
+        [output_directory '/noise_corrected_' I.similarity_metric ...
+        '_nsmps' num2str(I.n_bootstrap_smps) '_fixrandseed-v2.mat'];
+    else
+        bootstrap_metric_MAT_file = ...
+            [output_directory '/' I.similarity_metric ...
+            '_nsmps' num2str(I.n_bootstrap_smps) '_fixrandseed-v2.mat'];
+        error('Need to finish implementing this code without noise correction');
+    end
+    
+    % delete file if is un-loadable
+    if exist(bootstrap_metric_MAT_file, 'file')
+        try
+            load(bootstrap_metric_MAT_file, 'r_bootstrap');
+        catch
+            delete(bootstrap_metric_MAT_file);
+        end
+    end
+    
+    if ~exist(bootstrap_metric_MAT_file, 'file') || I.overwrite_correlations
+        
+        % load variables from previous step
+        load(prediction_MAT_file, 'Yh', 'D', 'folds', 'nonempty_voxels', ...
+            'n_stim', 'n_rep', 'n_vox', 'n_nonempty_vox');
+        
+        % separate out reps / voxels into different dimensions
+        D = reshape(D, [n_stim, n_rep, n_nonempty_vox]);
+        Yh = reshape(Yh, [n_stim, n_rep, n_nonempty_vox]);
+        
+        if ~I.within_fold_correlation
+            folds = ones(size(folds));
+        end
+        
+        r_bootstrap = nan(I.n_bootstrap_smps, n_nonempty_vox);
+        if isempty(I.bstrap_smps)
+            ResetRandStream2(0);
+            I.bstrap_smps = randi(size(D,1), [size(D,1), I.n_bootstrap_smps]);
+        end
+        for j = 1:I.n_bootstrap_smps
+            if mod(j, round(I.n_bootstrap_smps/50))==0
+                fprintf('Sample %d\n', j); drawnow;
+            end
+            for i = 1:size(D,3);
+                if I.noise_correct
+                    switch I.correction_method
+                        case 'correlation-based'
+                            r_bootstrap(j,i) = normalized_correlation_within_folds(...
+                                D(I.bstrap_smps(:,j),:,i), Yh(I.bstrap_smps(:,j),:,i), folds(I.bstrap_smps(:,j)), ...
+                                'metric', I.similarity_metric, ...
+                                'only_cross_column_corr', I.only_cross_column_corr);
+                        case 'variance-based'
+                            r_bootstrap(j,i) = noise_corrected_similarity_within_folds(...
+                                D(I.bstrap_smps(:,j),:,i), Yh(I.bstrap_smps(:,j),:,i), folds(I.bstrap_smps(:,j)), ...
+                                'metric', I.similarity_metric,...
+                                'only_cross_column_cov', I.only_cross_column_corr);
+                        otherwise
+                            error('Switch statement fell through');
+                    end
+                else
+                    error('Need to implement similarity metric without correction');
+                end
+            end
+        end
+        
+        % add back in empty voxels as NaNs
+        r_bootstrap = fillin_NaN(r_bootstrap, nonempty_voxels, 2);
+        
+        save(bootstrap_metric_MAT_file, 'r_bootstrap');
+        
+    else
+        
+        load(bootstrap_metric_MAT_file, 'r_bootstrap');
+        
+    end
+end
+
+%% Reformating
+
+% convert similarity metric to grid
+Gsim = G;
+Gsim.grid_data{1} = nan(size(Gsim.grid_data{1},1), size(Gsim.grid_data{1},2));
+Gsim.grid_data{2} = nan(size(Gsim.grid_data{2},1), size(Gsim.grid_data{2},2));
+Gsim = matrix2grid(r, Gsim);
+save(similarity_metric_MAT_file, '-append', 'Gsim');
+
+% convert similarity metric to grid
+if I.n_bootstrap_smps > 0
+    Gbootstrap = G;
+    Gbootstrap.grid_data{1} = nan(size(Gbootstrap.grid_data{1},1), size(Gbootstrap.grid_data{1},2));
+    Gbootstrap.grid_data{2} = nan(size(Gbootstrap.grid_data{2},1), size(Gbootstrap.grid_data{2},2));
+    Gbootstrap = matrix2grid_v2(r_bootstrap, Gbootstrap);
+    save(bootstrap_metric_MAT_file, '-append', 'Gbootstrap');
+else
+    Gbootstrap = struct([]);
+    bootstrap_metric_MAT_file = '';
 end
 
 %% Permutation test
@@ -231,15 +365,6 @@ end
 %     save(similarity_metric_MAT_file, '-append', 'sig');
 % 
 % end
-
-%% Reformating
-
-% convert similarity metric to grid
-Gsim = G;
-Gsim.grid_data{1} = nan(size(Gsim.grid_data{1},1), size(Gsim.grid_data{1},2));
-Gsim.grid_data{2} = nan(size(Gsim.grid_data{2},1), size(Gsim.grid_data{2},2));
-Gsim = matrix2grid(r, Gsim);
-save(similarity_metric_MAT_file, '-append', 'Gsim');
 
 % % convert significance metric to grid
 % if I.n_perms > 0
