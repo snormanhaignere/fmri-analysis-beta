e04432e9c381e1d6c993c867e33fd8917237f275
Sam Norman-Haignere <snormanhaignere@gmail.com>
2018-05-22 12:57:07 +0200

    convert_anatomical_dicoms_to_nifti : small update

--- code/convert_anatomical_dicoms_to_nifti.m
+++ code/convert_anatomical_dicoms_to_nifti.m
@@ -1,6 +1,8 @@
 function convert_anatomical_dicoms_to_nifti(exp, us, varargin)
 
 % 2016-08-27: Modified how optional arguments are handled
+% 
+% 2018-04-08: Changed how the first dicom file was read in
 
 global root_directory
 
@@ -20,13 +22,13 @@ end
 
 % dicoms directory
 dicoms_directory = [root_directory '/' exp '/data/brain/dicoms/usub' num2str(us) '_scan' num2str(scanid)];
-first_dicom_file = dir([dicoms_directory '/' '*-1-1.dcm']);
+first_dicom_file = dir([dicoms_directory '/' '*-' num2str(seqid) '-1.dcm']);
 if length(first_dicom_file)~= 1;
     error('Error: problem reading first dicom file');
 end
 
 % convert file
-reference_dicom_file = [dicoms_directory '/' strrep(first_dicom_file.name, '-1-1', ['-' num2str(seqid) '-1'])];
+reference_dicom_file = [dicoms_directory '/' first_dicom_file.name]; %strrep(first_dicom_file.name, '-1-1', ['-' num2str(seqid) '-1'])];
 if ~exist(anatomical_file,'file') || I.overwrite
     fprintf(['mri_convert --in_type siemens_dicom --out_type nii '  reference_dicom_file '  ' anatomical_file '\n']);
     unix(['mri_convert --in_type siemens_dicom --out_type nii '  reference_dicom_file '  ' anatomical_file]);
